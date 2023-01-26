409e3040bbf9bb78b7a1b80a200cd90beb6bc0c8
Jo9085 <josephine.pean@gmail.com>
2023-01-26 22:27:36 +0100

    almost

--- lib/mairie.rb
+++ lib/mairie.rb
@@ -13,18 +13,67 @@ def get_townhall_email(townhall_URL)
 
 end
 
-get_townhall_email("https://www.annuaire-des-mairies.com/95/avernes.html")
+#get_townhall_email("https://www.annuaire-des-mairies.com/95/avernes.html")
+
+
+def get_cities(region_URL)
+
+    page_1region = Nokogiri::HTML(URI.open(region_URL))
+
+    #puts page_1region.xpath('//a[@class="lientxt"]').each do |node|
+     # puts node.text
+    #end
+    final_cities=[]
+    cities = page_1region.css("a").select{|link| link['class'] == "lientxt"}
+    #cities.each{|link| puts "#{link.text}"} 
+    #cities.each{|link| final_cities << link.text}
+    (0..cities.length-1).each do |i|
+        final_cities << cities[i].text
+    end
+    return final_cities
+
+end
 
 
 def get_townhall_urls(region_URL)
 
     page_1region = Nokogiri::HTML(URI.open(region_URL))
 
-    puts page_1region.xpath('//a[@class="lientxt"]').each do |node|
-      puts node.text
+    town_url = page_1region.xpath('//a[@class="lientxt"]/@href')
+    town_url_clean=[]
+
+    #(0..arraytest.length-1).each do |i|
+     #   arraytest[i] = arraytest[i].gsub("./", "")
+    #end  
+    (0..town_url.length).each do |i|  
+        town_url_clean[i] = 'https://www.annuaire-des-mairies.com/' + "#{town_url[i]}"
+        town_url_clean[i] = town_url_clean[i].gsub("./","")
+        #puts town_url_clean[i]
     end
-    
+
+    return town_url_clean
+   
+ 
+    #cities = page_1region.css("a").select{|link| link['class'] == "lientxt"}
+    #cities.each{|link| puts "#{link['href']}"}    
 
 end
 
 #get_townhall_urls("https://www.annuaire-des-mairies.com/val-d-oise.html")
+
+def get_all_emails(i)
+    #final_cities = get_cities("https://www.annuaire-des-mairies.com/val-d-oise.html")
+
+    #emails = []
+
+    #(0..final_cities.length).each do |i|
+       
+    #     emails [i] = get_townhall_email(get_townhall_urls("https://www.annuaire-des-mairies.com/val-d-oise.html")[i])
+    #end
+
+    #return emails
+
+    return get_townhall_email(get_townhall_urls("https://www.annuaire-des-mairies.com/val-d-oise.html")[i])
+end
+
+puts get_all_emails (6)
--- spec/mairie_spec.rb
+++ spec/mairie_spec.rb
@@ -1,8 +1,34 @@
 require_relative '../lib/mairie.rb'
 townhall_URL='https://www.annuaire-des-mairies.com/95/avernes.html'
+region_URL="https://www.annuaire-des-mairies.com/val-d-oise.html"
 
 describe "the get_townhall_email method" do
     it "should return the townhall email" do
       expect(get_townhall_email(townhall_URL)).to eq("mairie.avernes@orange.fr")
+    end
+end
+
+
+describe "the get_cities method" do
+    it "should return something" do
+      expect(get_cities(region_URL)).to_not be_nil
+    end
+    it "should return an array" do
+        expect(get_cities(region_URL).class).to eq(Array)
+    end
+    it "should return at least a given city" do
+        expect(get_cities(region_URL)).to include("VEMARS")
+    end
+end
+
+describe "the get_townhall_urls method" do
+    it "should return something" do
+      expect(get_townhall_urls(region_URL)).to_not be_nil
+    end
+    it "should return an array" do
+        expect(get_townhall_urls(region_URL).class).to eq(Array)
+    end
+    it "should return at least a given city" do
+        expect(get_townhall_urls(region_URL)).to include("VEMARS")
     end
 end
